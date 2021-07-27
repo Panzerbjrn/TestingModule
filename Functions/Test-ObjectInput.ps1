0076b29ecd910cc6911b018d223daf3bc40857b7
Lars.Petersson <lars@panzerbjrn.eu>
2021-07-27 12:05:22 +0200

    Updated

---
+++ Functions/Test-ObjectInput.ps1
@@ -0,0 +1,25 @@
+Function Test-ObjectInput
+{
+Param (
+	[Parameter(Mandatory)][psobject]$PSObjectInput
+	)
+
+	$HashTable = @()
+	Write-Verbose "PSObjectInput is: $PSObjectInput"
+	[int]$I = 1
+<#	ForEach ($Item in $PSObjectInput)
+	{
+		Write-Verbose "$I"
+		$I++
+		Write-Verbose "$Item"
+		$HashTable += @(
+			[pscustomobject]@{
+				ArrayItem = $Item
+				ItemNumber = $I
+			}
+		)
+	}
+#>
+	$PSObjectInput
+}
+
---
+++ Functions/Test-WhatIf.ps1
@@ -0,0 +1,45 @@
+Function Test-WhatIf {
+	[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='High')]
+	param(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$FilePath
+	)
+	
+	BEGIN{
+		Write-Verbose "#################################################################"
+		Write-Verbose "Beginning $($MyInvocation.MyCommand.Name) on $($ENV:ComputerName) @ $(Get-Date -Format "yyyy.MM.dd HH:mm:ss")"
+		Write-Verbose "#################################################################"
+
+		Write-Verbose "Checking to see if the file provided already exists"
+		$FileExists = Test-Path -Path $FilePath
+	}
+		
+	PROCESS{
+		TRY{
+			IF($FileExists){
+				Write-Warning "$FilePath already exists and will be overwritten"
+				IF($PSCmdlet.ShouldProcess(
+						("Overwritting existing file {0}" -f $FilePath),
+						("Would you like to overwrite {0}?" -f $FilePath),
+						"Create File Prompt"
+					)){
+					$File = New-Item -Path $FilePath -ItemType File -Force -ErrorAction Stop
+				}
+			}
+			ELSE{
+				Write-Verbose "Creating $FilePath"
+				IF($PSCmdlet.ShouldProcess($FilePath,"Create File")){
+					$File = New-Item -Path $FilePath -ItemType File -Force -ErrorAction Stop
+				}
+			}
+		}
+		CATCH{
+			Throw "$($_.Exception.Message)"
+		}
+	}
+	END{
+		Write-Verbose "New file details"
+		Return $File
+	}
+}
