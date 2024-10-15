698c982e0b364c5ebc4d43319dda99211000e9b6
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2024-10-15 12:01:16 +0100

    Refreshing

--- TestingModule/Functions/Test-PSBoundParameters.ps1
+++ TestingModule/Functions/Test-PSBoundParameters.ps1
@@ -1,5 +1,5 @@
 Function Test-PSBoundParameters {
-    	<#
+    <#
 		.SYNOPSIS
 			Describe the function here
 
@@ -13,24 +13,24 @@ Function Test-PSBoundParameters {
 	[CmdletBinding()]
 
     param(
-      [string]$Text,
-      [int]$Number
-   )
+        [string]$Text,
+        [int]$Number
+    )
 
-   # Display all the passed parameters:
-   $PSBoundParameters
+    # Display all the passed parameters:
+    $PSBoundParameters
 
-   # or with a switch statement:
-   switch ($PSBoundParameters.Keys) {
+    # or with a switch statement:
+    switch ($PSBoundParameters.Keys) {
             'Text' { write-output ' A value for Text was supplied' }
             'Number' { write-output ' A value for Number was supplied'  }
-       }
+    }
 
-   # or looping through all the key/value pairs
-   foreach($boundparam in $PSBoundParameters.GetEnumerator()) {
-       "Key={0} Value={1}" -f $boundparam.Key,$boundparam.Value
-   }
+    # or looping through all the key/value pairs
+    foreach($boundparam in $PSBoundParameters.GetEnumerator()) {
+        "Key={0} Value={1}" -f $boundparam.Key,$boundparam.Value
+    }
 
-   # or Call a second function passing all the parameters plus any extra if needed:
-   Test-PSBoundParametersPriv @PSBoundParameters -ExtraDemoParam 'Testing 123'
+    # or Call a second function passing all the parameters plus any extra if needed:
+    #Test-PSBoundParametersPriv @PSBoundParameters -ExtraDemoParam 'Testing 123'
 }
